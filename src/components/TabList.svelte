4bfaf5bbda1119c516f629473af39b9bfe0d582b
capricorn <8250820+capricorn@users.noreply.github.com>
2023-05-21 15:49:17 -0600

    Add button to close selected tabs

--- src/components/TabList.svelte
+++ src/components/TabList.svelte
@@ -36,12 +36,25 @@
         }
     }
 
+    function closeSelectedTabs() {
+        for (const tabId of selectedTabIds) {
+            console.log('Closing tab: ' + tabId);
+            chrome.tabs.remove(tabId);
+        }
+
+        selectedTabIds = new Set();
+        tabs = [];
+        // WIP: Reloading tab list after deletion 
+        fetchTabs().then(() => console.log('Reloaded tab list.'));
+    }
+
     onMount(async () => {
         await fetchTabs();
     })
 </script>
 
 <div class="container">
+    <button on:click={closeSelectedTabs}>Close selected</button>
     {#each tabs as tab (tab.id)}
         <div class="tab" on:click={() => selectTab(tab)} style="background-color: {selectedTabIds.has(tab.id) ? "orange" : "white"}">{tab.title}</div>
     {/each}
