77890f45ee1e7d2ec930c906371847946052a5a9
capricorn <8250820+capricorn@users.noreply.github.com>
2023-05-31 21:32:20 -0600

    Underline hovered tab when meta key is held

--- src/components/TabList.svelte
+++ src/components/TabList.svelte
@@ -5,11 +5,37 @@
 
     let tabs: Tab[] = [];
     let selectedTabIds: Set<number> = new Set();
+    let metaKeyPressed = false;
+    let hoveredTab: Tab = undefined;
 
     $: selections = Array.from(selectedTabIds).sort()
     $: {
         console.log('selections');
         console.log(selectedTabIds);
+    }
+
+    function mouseOverTab(tab: Tab) {
+        console.log('mouse over tab. meta pressed: ' + metaKeyPressed);
+        if (metaKeyPressed) {
+            hoveredTab = tab;
+        }
+    }
+
+    function handleKeyDown(event: KeyboardEvent) {
+        if (event.key == 'Meta') {
+            console.log('meta down');
+            metaKeyPressed = true;
+        }
+    }
+
+    function handleKeyUp(event: KeyboardEvent) {
+        console.log('up: ' + event.key);
+        console.log('up: ' + event.metaKey);
+        if (event.key == 'Meta') {
+            console.log('meta up');
+            metaKeyPressed = false;
+            hoveredTab = undefined;
+        }
     }
 
     // TODO: Place in onMount
@@ -51,11 +77,16 @@
     })
 </script>
 
-<div class="container">
+<svelte:window on:keydown={handleKeyDown} on:keyup={handleKeyUp}/>
+
+<div class="container" >
     <h1>{tabs.length} {tabs.length == 1 ? "Tab" : "Tabs"}</h1>
     <button on:click={closeSelectedTabs}>Close selected</button>
     {#each tabs as tab (tab.id)}
-        <div class="tab" on:click={() => selectTab(tab)} style="background-color: {selectedTabIds.has(tab.id) ? "orange" : "white"}">{tab.title}</div>
+        <div class="{hoveredTab?.id == tab.id ? "tab hovered_tab" : "tab"}"
+            on:click={() => selectTab(tab)} 
+            on:mouseover={() => mouseOverTab(tab)}
+            style="background-color: {selectedTabIds.has(tab.id) ? "orange" : "white"}">{tab.title}</div>
     {/each}
 </div>
 
@@ -64,5 +95,10 @@
         font-size: 24px;
         padding-top: 4px;
         padding-bottom: 4px;
+    }
+
+    .hovered_tab {
+        text-decoration: underline;
+        cursor: pointer;
     }
 </style>
