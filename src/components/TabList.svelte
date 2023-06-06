597ab35b1cb2f51ce646cec5a0610bf84947e73f
capricorn <8250820+capricorn@users.noreply.github.com>
2023-06-05 20:27:06 -0600

    WIP handling of shift multiselection

--- src/components/TabList.svelte
+++ src/components/TabList.svelte
@@ -1,12 +1,16 @@
 <script lang='ts'>
     import { onMount } from "svelte";
+    import * as _ from "lodash";
 
     type Tab = chrome.tabs.Tab;
 
     let tabs: Tab[] = [];
     let selectedTabIds: Set<number> = new Set();
     let metaKeyPressed = false;
+    let shiftKeyPressed = false;
     let hoveredTab: Tab = undefined;
+
+    let lastTabSelected: Tab = undefined;
 
     $: selections = Array.from(selectedTabIds).sort()
     $: {
@@ -16,7 +20,7 @@
 
     function mouseOverTab(tab: Tab) {
         console.log('mouse over tab. meta pressed: ' + metaKeyPressed);
-        if (metaKeyPressed) {
+        if (metaKeyPressed || shiftKeyPressed) {
             hoveredTab = tab;
         }
     }
@@ -25,6 +29,9 @@
         if (event.key == 'Meta') {
             console.log('meta down');
             metaKeyPressed = true;
+        } else if (event.key == 'Shift') {
+            console.log('shift pressed');
+            shiftKeyPressed = true;
         }
     }
 
@@ -34,8 +41,12 @@
         if (event.key == 'Meta') {
             console.log('meta up');
             metaKeyPressed = false;
-            hoveredTab = undefined;
+        } else if (event.key == 'Shift') {
+            console.log('shift up');
+            shiftKeyPressed = false;
         }
+
+        hoveredTab = undefined;
     }
 
     // TODO: Place in onMount
@@ -49,9 +60,31 @@
             console.log('switching to tab');
             chrome.tabs.update(tab.id, { active: true })
             chrome.windows.update(tab.windowId, { focused: true })
+        } else if (shiftKeyPressed) {
+            console.log('shift select occurring')
+            // TODO: Better optional handling?
+            let startIndex = tabs.map(f => f.id).indexOf(lastTabSelected!.id);
+            let endIndex = tabs.map(f => f.id).indexOf(tab.id);
+
+            let selectedTabIdsRegion = Array.from(tabs.entries())
+                .filter(val => (val[0] >= startIndex && val[0] <= endIndex))
+                .map(f => f[1].id);
+            
+            /*
+            let tabIdsToRemove = selectedTabIdsRegion.filter(f => selectedTabIds.has(f));
+
+            console.log('tab region: ' + selectedTabIdsRegion);
+
+            selectedTabIds = new Set(_.union(
+                _.difference(...selectedTabIds, tabIdsToRemove),
+                selectedTabIdsRegion
+            ));
+            */
+            selectedTabIds = new Set([...selectedTabIds, ...selectedTabIdsRegion]);
         } else if (selectedTabIds.has(tab.id)) {
             selectedTabIds.delete(tab.id);
         } else {
+            lastTabSelected = tab;
             selectedTabIds.add(tab.id);
         }
 
