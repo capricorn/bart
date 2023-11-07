4071a2ed59563023f8f8e0e790ae45958992b5da
capricorn <8250820+capricorn@users.noreply.github.com>
2023-11-06 22:44:21 -0700

    PoC of nested menus

--- src/components/ContextMenu.svelte
+++ src/components/ContextMenu.svelte
@@ -5,6 +5,21 @@
     export let y: number;
 
     let menu: HTMLElement;
+    let options = [ 'Move', 'Close', 'Clear filter' ];
+
+    let submenu = false;
+
+    let width: number;
+    let height: number;
+
+    function handleMenuSelection(option: string) {
+        console.log('mouse over menu: ' + option);
+
+        // TODO: Better approach
+        if (option == 'Move') {
+            submenu = true;
+        }
+    }
 
     onMount(() => {
         menu.style.left = x + 'px';
@@ -12,14 +27,26 @@
     })
 </script>
 
-<div id="bart-context-menu" bind:this={menu}>
+<div id="bart-context-menu" bind:this={menu} bind:clientWidth={width} bind:clientHeight={height}>
+    {#each options as option}
+    <div class="bart-context-menu-option" on:mouseover={() => handleMenuSelection(option)}>{option}</div>
+    {/each}
 </div>
+
+{#if submenu}
+<svelte:self x={x+width} y={y}/>
+{/if}
 
 <style>
     #bart-context-menu {
         position: absolute;
         width: 100px;
-        height: 100px;
-        background-color: black;
+        border-width: 1px;
+        border-color: black;
+        border-style: solid;
+    }
+
+    .bart-context-menu-option:hover {
+        background-color: lightgray;
     }
 </style>
