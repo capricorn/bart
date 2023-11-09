51326b28c71eec52613f283eac7179b9f48bbdd3
capricorn <8250820+capricorn@users.noreply.github.com>
2023-11-08 20:43:57 -0700

    Correctly draw select rectangle no matter coord start/end

--- src/components/TabList.svelte
+++ src/components/TabList.svelte
@@ -55,6 +55,22 @@
         return Math.abs(tabSelectStartCoord[1] - tabSelectEndCoord[1]);
     })();
 
+    $: tabSelectTopPx = (() => {
+        if (tabSelectStartCoord == undefined || tabSelectEndCoord == undefined) {
+            return undefined;
+        }
+
+        return Math.min(tabSelectStartCoord[1], tabSelectEndCoord[1]);
+    })();
+
+    $: tabSelectLeftPx = (() => {
+        if (tabSelectStartCoord == undefined || tabSelectEndCoord == undefined) {
+            return undefined;
+        }
+
+        return Math.min(tabSelectStartCoord[0], tabSelectEndCoord[0]);
+    })();
+
     $: {
         console.log('region width: ' + tabSelectRegionWidth);
         console.log('region height: ' + tabSelectRegionHeight);
@@ -562,8 +578,7 @@
 {/if}
 
 {#if tabSelectStartCoord != undefined && tabSelectEndCoord != undefined}
-<!-- TODO: Only works in one direction -->
-<div id="tab-selection-div" style="left: {tabSelectStartCoord[0]}px; top: {tabSelectStartCoord[1]}px; width: {tabSelectRegionWidth}px; height: {tabSelectRegionHeight}px;"></div>
+<div id="tab-selection-div" style="left: {tabSelectLeftPx}px; top: {tabSelectTopPx}px; width: {tabSelectRegionWidth}px; height: {tabSelectRegionHeight}px;"></div>
 {/if}
 
 <style>
